e6ce5c5dbe4ec627159ef7cb7ac4ead44ca4384a
Matteo Cirami <mcirami@users.noreply.github.com>
2021-12-07 11:19:29 -0600

    added icon object array

---
+++ resources/js/Services/IconObjects.js
@@ -0,0 +1,322 @@
+export const icons = [
+    {
+        "name" : "Amazon Music",
+        "required_in_url": "music.amazon",
+    },
+    {
+        "name" : "Among Us",
+        "required_in_url": null,
+    },
+    {
+        "name" : "AppleMusic",
+        "required_in_url": "music.apple",
+    },
+    {
+        "name" : "AutoTrader",
+        "required_in_url": "autotrader",
+    },
+    {
+        "name" : "Behance",
+        "required_in_url": "behance",
+    },
+    {
+        "name" : "Big Cartel",
+        "required_in_url": "bigcartel",
+    },
+    {
+        "name" : "CarGurus",
+        "required_in_url": "cargurus",
+    },
+    {
+        "name" : "Cars",
+        "required_in_url": "cars",
+    },
+    {
+        "name" : "CashApp",
+        "required_in_url": "cash.app",
+    },
+    {
+        "name" : "Craigslist",
+        "required_in_url": "craigslist",
+    },
+    {
+        "name" : "Depop",
+        "required_in_url": "depop",
+    },
+    {
+        "name" : "Discord",
+        "required_in_url": "discord",
+    },
+    {
+        "name" : "DoorDash",
+        "required_in_url": "doordash",
+    },
+    {
+        "name" : "Dropbox",
+        "required_in_url": "dropbox",
+    },
+    {
+        "name" : "Ebay",
+        "required_in_url": "ebay",
+    },
+    {
+        "name" : "EbayMotors",
+        "required_in_url": "ebay",
+    },
+    {
+        "name" : "Etsy",
+        "required_in_url": "etsy",
+    },
+    {
+        "name" : "Facebook",
+        "required_in_url": "facebook",
+    },
+    {
+        "name" : "Facetime",
+        "required_in_url": null,
+    },
+    {
+        "name" : "Fiverr",
+        "required_in_url": "fiverr",
+    },
+    {
+        "name" : "Flickr",
+        "required_in_url": "flickr",
+    },
+    {
+        "name" : "Gmail",
+        "required_in_url": "mail.google",
+    },
+    {
+        "name" : "Google Drive",
+        "required_in_url": "drive.google",
+    },
+    {
+        "name" : "Google Earth",
+        "required_in_url": "earth.google",
+    },
+    {
+        "name" : "Google Photos",
+        "required_in_url": "photos.google",
+    },
+    {
+        "name" : "GoogleChat",
+        "required_in_url": null,
+    },
+    {
+        "name" : "GoogleMaps",
+        "required_in_url": "maps.google",
+    },
+    {
+        "name" : "GrubHub",
+        "required_in_url": "grubhub",
+    },
+    {
+        "name" : "ICQ",
+        "required_in_url": "icq",
+    },
+    {
+        "name" : "InstaCart",
+        "required_in_url": "instacart",
+    },
+    {
+        "name" : "Instagram",
+        "required_in_url": "instagram",
+    },
+    {
+        "name" : "Life360",
+        "required_in_url": "life360",
+    },
+    {
+        "name" : "Linkedin",
+        "required_in_url": "linkedin",
+    },
+    {
+        "name" : "Marketplace",
+        "required_in_url": null,
+    },
+    {
+        "name" : "Medium",
+        "required_in_url": "medium",
+    },
+    {
+        "name" : "Messenger",
+        "required_in_url": "messenger",
+    },
+    {
+        "name" : "MicrosoftTeams",
+        "required_in_url": null,
+    },
+    {
+        "name" : "OfferUp",
+        "required_in_url": "offerUp",
+    },
+    {
+        "name" : "OneDrive",
+        "required_in_url": null,
+    },
+    {
+        "name" : "OnlyFans",
+        "required_in_url": "onlyfans",
+    },
+    {
+        "name" : "OpenTable",
+        "required_in_url": "opentable",
+    },
+    {
+        "name" : "Outlook",
+        "required_in_url": "outlook.live",
+    },
+    {
+        "name" : "Pandora",
+        "required_in_url": "pandora",
+    },
+    {
+        "name" : "Patreon",
+        "required_in_url": "patreon",
+    },
+    {
+        "name" : "Paypal",
+        "required_in_url": "paypal",
+    },
+    {
+        "name" : "PhotoBucket",
+        "required_in_url": "photobucket",
+    },
+    {
+        "name" : "Picasa",
+        "required_in_url": null,
+    },
+    {
+        "name" : "Picasa",
+        "required_in_url": null,
+    },
+    {
+        "name" : "Pinterest",
+        "required_in_url": "pinterest",
+    },
+    {
+        "name" : "Postmates",
+        "required_in_url": "postmates",
+    },
+    {
+        "name" : "Quora",
+        "required_in_url": "quora",
+    },
+    {
+        "name" : "RSS Feed",
+        "required_in_url": "rss",
+    },
+    {
+        "name" : "Roblox",
+        "required_in_url": "roblox",
+    },
+    {
+        "name" : "Shopify",
+        "required_in_url": "shopify",
+    },
+    {
+        "name" : "Slack",
+        "required_in_url": "slack",
+    },
+    {
+        "name" : "Spotify",
+        "required_in_url": "spotify",
+    },
+    {
+        "name" : "Telegram",
+        "required_in_url": "telegram",
+    },
+    {
+        "name" : "TikTok",
+        "required_in_url": "tiktok",
+    },
+    {
+        "name" : "TripAdvisor",
+        "required_in_url": "tripAdvisor",
+    },
+    {
+        "name" : "Tumblr",
+        "required_in_url": "tumblr",
+    },
+    {
+        "name" : "Twitch",
+        "required_in_url": "twitch.tv",
+    },
+    {
+        "name" : "Twitter",
+        "required_in_url": "twitter",
+    },
+    {
+        "name" : "UberEats",
+        "required_in_url": "ubereats",
+    },
+    {
+        "name" : "UpWork",
+        "required_in_url": "upwork",
+    },
+    {
+        "name" : "Venmo",
+        "required_in_url": "venmo",
+    },
+    {
+        "name" : "Viber",
+        "required_in_url": "viber",
+    },
+    {
+        "name" : "Vimeo",
+        "required_in_url": "vimeo",
+    },
+    {
+        "name" : "Whatsapp",
+        "required_in_url": "whatsapp",
+    },
+    {
+        "name" : "Wikipedia",
+        "required_in_url": "wikipedia",
+    },
+    {
+        "name" : "Yahoo Mail",
+        "required_in_url": "mail.yahoo",
+    },
+    {
+        "name" : "Yelp",
+        "required_in_url": "yelp",
+    },
+    {
+        "name" : "YouTube",
+        "required_in_url": "youtube",
+    },
+    {
+        "name" : "Zelle",
+        "required_in_url": "zellepay",
+    },
+    {
+        "name" : "Airbnb",
+        "required_in_url": "airbnb",
+    },
+    {
+        "name" : "Poshmark",
+        "required_in_url": "poshmark",
+    },
+    {
+        "name" : "Reddit",
+        "required_in_url": "reddit",
+    },
+    {
+        "name" : "Snapchat",
+        "required_in_url": "snapchat",
+    },
+    {
+        "name" : "Soundcloud",
+        "required_in_url": "soundcloud",
+    },
+    {
+        "name" : "Xbox",
+        "required_in_url": null,
+    },
+    {
+        "name" : "Zillow",
+        "required_in_url": "zillow",
+    },
+]
