399a6ae107395dddadae4a7cdeecd79f589733b5
Matteo Cirami <mcirami@users.noreply.github.com>
2021-12-10 12:39:32 -0600

    updated voyager settings and css

--- config/voyager.php
+++ config/voyager.php
@@ -13,7 +13,7 @@ return [
     'user' => [
         'add_default_role_on_register' => true,
         'default_role'                 => 'user',
-        'default_avatar'               => 'users/cool-skull.jpg',
+        'default_avatar'               => 'https://lp-production-images.s3.us-east-2.amazonaws.com/settings/December2021/cool-skull.jpg',
         'redirect'                     => '/admin',
     ],
 
