29be970378fe99160a29df307f98e20bc02334a5
Matteo Cirami <mcirami@users.noreply.github.com>
2021-12-21 16:25:04 -0600

    fixed issued with saving click if not logged in

--- app/Models/LinkVisit.php
+++ app/Models/LinkVisit.php
@@ -10,12 +10,13 @@ class LinkVisit extends Model
     use HasFactory;
 
     /**
-     * The attributes that are not mass assignable.
+     * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $guarded = [
-        'link_id'
+    protected $fillable = [
+        'link_id',
+        'page_id',
     ];
 
     public function link() {
