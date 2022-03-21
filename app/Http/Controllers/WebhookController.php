095a1e8b10c70a5f46eacad162472d08e0500258
Matteo Cirami <mcirami@users.noreply.github.com>
2022-03-21 09:32:59 -0500

    fixed code to get subscrition after webhook trigger

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -7,7 +7,7 @@ use App\Models\Subscription;
 use App\Models\User;
 use Illuminate\Support\Facades\Log;
 use App\Http\Traits\SubscriptionTrait;
-
+use Braintree\WebhookNotification;
 
 class WebhookController extends Controller
 {
@@ -19,60 +19,65 @@ class WebhookController extends Controller
         $gateway = $this->createGateway();
 
         /* FOR TESTING */
-        /*$notification = $gateway->webhookTesting()->sampleNotification(
+        /*$sampleNotification = $gateway->webhookTesting()->sampleNotification(
             WebhookNotification::SUBSCRIPTION_CANCELED,
             'my_id'
-        );*/
-
-        /*$webhookNotification = $gateway->webhookNotification()->parse(
-            $notification['bt_signature'],
-            $notification['bt_payload']
-        );*/
-
-        $notification = $gateway->webhookNotification()->parse(
-            $_POST["bt_signature"],
-            $_POST["bt_payload"]
         );
 
-        if ($notification->kind === 'subscription_expired') {
-            $subscription = Subscription::findOrFail($notification->subscription->id);
-            $user = User::findOrFail($subscription->user_id);
-            $userPages = $user->pages()->get();
+        $notification = $gateway->webhookNotification()->parse(
+            $sampleNotification['bt_signature'],
+            $sampleNotification['bt_payload']
+        );*/
 
-            if (!empty($userPages)) {
+        if (
+            isset($_POST["bt_signature"]) &&
+            isset($_POST["bt_payload"])
+        ) {
+            $notification = $gateway->webhookNotification()->parse(
+                $_POST["bt_signature"],
+                $_POST["bt_payload"]
+            );
 
-                foreach ( $userPages as $userPage ) {
-                    if ( $userPage->is_protected ) {
-                        $userPage->is_protected = 0;
-                        $userPage->password = null;
-                    }
-                    if (!$userPage->default) {
-                        $userPage->disabled = true;
-                    }
+            if ( $notification->kind === 'subscription_expired' ) {
+                $subscription = Subscription::where('braintree_id', $notification->subscription->id )->first();
+                $user         = User::findOrFail( $subscription->user_id );
+                $userPages    = $user->pages()->get();
 
-                    $userPage->save();
+                if ( !empty( $userPages ) ) {
 
-                    if($userPage->default) {
-                        $folders = Folder::where( 'page_id', $userPage->id )->get();
-                        if ( !empty( $folders ) ) {
-                            foreach ( $folders as $folder ) {
-                                if ( $folder->active_status ) {
-                                    $folder->active_status = 0;
-                                    $folder->save();
+                    foreach ( $userPages as $userPage ) {
+                        if ( $userPage->is_protected ) {
+                            $userPage->is_protected = 0;
+                            $userPage->password     = null;
+                        }
+                        if ( ! $userPage->default ) {
+                            $userPage->disabled = true;
+                        }
+
+                        $userPage->save();
+
+                        if ( $userPage->default ) {
+                            $folders = Folder::where( 'page_id', $userPage->id )->get();
+                            if ( ! empty( $folders ) ) {
+                                foreach ( $folders as $folder ) {
+                                    if ( $folder->active_status ) {
+                                        $folder->active_status = 0;
+                                        $folder->save();
+                                    }
                                 }
                             }
                         }
                     }
                 }
+
+                $subscription->update( [ 'name', 'free' ] );
             }
 
-            $subscription->update(['name', 'free']);
+            Log::channel('webhooks')->info($notification->timestamp->format('D M j G:i:s T Y') . " --- " . $notification->kind . " --- " . $notification->subscription->id);
+
+            //Log::channel( 'webhooks' )->info( $notification->subscription->id . " --- " . $notification->kind );
+
+            header( "HTTP/1.1 200 OK" );
         }
-
-        Log::channel('webhooks')->info($notification->timestamp->format('D M j G:i:s T Y') . " --- " . $notification->kind . " --- " . $notification->subscription->id);
-
-        //Log::channel('webhooks')->info($webhookNotification->subscription->id . " --- " . $webhookNotification->kind);
-
-        header("HTTP/1.1 200 OK");
     }
 }
