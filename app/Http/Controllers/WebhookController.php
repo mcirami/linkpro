34995d9d34b6ddeddb427ecb757d32c01e07d2dc
Matteo Cirami <mcirami@users.noreply.github.com>
2022-05-31 12:33:54 -0500

    added log statements to webhook controller

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Folder;
 use App\Models\Subscription;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 use App\Http\Traits\SubscriptionTrait;
 use Braintree\WebhookNotification;
@@ -19,15 +20,31 @@ class WebhookController extends Controller
         $gateway = $this->createGateway();
 
         /* FOR TESTING */
+
         /*$sampleNotification = $gateway->webhookTesting()->sampleNotification(
             WebhookNotification::SUBSCRIPTION_EXPIRED,
             'my_id'
         );
 
+
         $notification = $gateway->webhookNotification()->parse(
             $sampleNotification['bt_signature'],
             $sampleNotification['bt_payload']
-        );*/
+        );
+
+        if ( $notification->kind === 'subscription_expired' ) {
+            Log::channel( 'webhooks' )->info( Carbon::now() .
+                                              " --- bt_signature --- " .
+                                              $sampleNotification['bt_signature'] .
+                                              " --- bt_payload --- " .
+                                              $sampleNotification['bt_payload'] .
+                                              "--- kind --- " .
+                                              $notification->kind
+            );
+        }*/
+        /******/
+
+        Log::channel('webhooks')->info(Carbon::now() . " --- bt_signature --- ". $_POST["bt_signature"] ." --- bt_payload --- " . $_POST["bt_payload"]);
 
         if (
             isset($_POST["bt_signature"]) &&
@@ -38,7 +55,9 @@ class WebhookController extends Controller
                 $_POST["bt_payload"]
             );
 
-            if ( $notification->kind === 'subscription_expired' ) {
+            Log::channel('webhooks')->info($notification->timestamp->format('D M j G:i:s T Y') .  "--- Before IF statement --- Kind: " . $notification->kind . " --- Sub ID:" . $notification->subscription->id);
+
+            if ( $notification->kind == 'subscription_expired' ) {
                 $subscription = Subscription::where('braintree_id', $notification->subscription->id )->first();
                 $user         = User::findOrFail( $subscription->user_id );
                 $userPages    = $user->pages()->get();
@@ -50,7 +69,7 @@ class WebhookController extends Controller
                             $userPage->is_protected = 0;
                             $userPage->password     = null;
                         }
-                        if ( ! $userPage->default ) {
+                        if ( !$userPage->default ) {
                             $userPage->disabled = true;
                         }
 
@@ -58,7 +77,7 @@ class WebhookController extends Controller
 
                         if ( $userPage->default ) {
                             $folders = Folder::where( 'page_id', $userPage->id )->get();
-                            if ( ! empty( $folders ) ) {
+                            if ( !empty( $folders ) ) {
                                 foreach ( $folders as $folder ) {
                                     if ( $folder->active_status ) {
                                         $folder->active_status = 0;
@@ -71,9 +90,11 @@ class WebhookController extends Controller
                 }
 
                 $subscription->update( [ 'name', 'free' ] );
+
+                Log::channel('webhooks')->info($notification->timestamp->format('D M j G:i:s T Y') . "--- Inside IF statement --- " . $notification->kind . " --- " . $notification->subscription->id);
+
             }
 
-            Log::channel('webhooks')->info($notification->timestamp->format('D M j G:i:s T Y') . " --- " . $notification->kind . " --- " . $notification->subscription->id);
 
             //Log::channel( 'webhooks' )->info( $notification->subscription->id . " --- " . $notification->kind );
 
