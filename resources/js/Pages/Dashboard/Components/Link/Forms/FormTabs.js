43c6b0442c1984be1349c7e16220cdb7000ea048
Matteo Cirami <mcirami@users.noreply.github.com>
2022-11-15 15:27:40 -0600

    removed integration tab inside folder

--- resources/js/Pages/Dashboard/Components/Link/Forms/FormTabs.js
+++ resources/js/Pages/Dashboard/Components/Link/Forms/FormTabs.js
@@ -82,7 +82,7 @@ const FormTabs = ({
     }
 
     return (
-        <div className="my_row radios_wrap">
+        <div className={ !folderID ? "my_row radios_wrap" : "my_row radios_wrap two_col" }>
             <div className={radioValue === "standard" ? "radio_wrap active" : "radio_wrap" }>
                 <label htmlFor="standard_radio">
                     <div className="radio_input_wrap">
@@ -106,19 +106,28 @@ const FormTabs = ({
                 </label>
                 {!subStatus && <span className="disabled_wrap" data-type="custom" onClick={(e) => handleOnClick(e)} />}
             </div>
-            <div className={radioValue === "integration" ? "radio_wrap active" : "radio_wrap" }>
-                <label htmlFor="integration">
-                    <div className="radio_input_wrap">
-                        <input id="integration" type="radio" value="integration" name="icon_type"
-                               onChange={(e) => { handleOnChange(e) }}
-                               disabled={!subStatus || folderID}
-                               checked={radioValue === "integration"}
-                        />
-                    </div>
-                    Integrations
-                </label>
-                { !subStatus || folderID ? <span className="disabled_wrap" data-type="integration" onClick={(e) => handleOnClick(e)} /> : ""}
-            </div>
+            {!folderID &&
+                <div className={radioValue === "integration" ?
+                    "radio_wrap active" :
+                    "radio_wrap"}>
+                    <label htmlFor="integration">
+                        <div className="radio_input_wrap">
+                            <input id="integration" type="radio" value="integration" name="icon_type"
+                                   onChange={(e) => {
+                                       handleOnChange(e)
+                                   }}
+                                   disabled={!subStatus}
+                                   checked={radioValue === "integration"}
+                            />
+                        </div>
+                        Integrations
+                    </label>
+                    {!subStatus &&
+                        <span className="disabled_wrap" data-type="integration" onClick={(e) => handleOnClick(
+                            e)}/>
+                    }
+                </div>
+            }
         </div>
     );
 };
